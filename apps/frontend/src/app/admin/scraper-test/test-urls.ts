2dc2f2ef011b20b19b4c168001f2bac7511dbf13
artemHushan <artem.hushan22@gmail.com>
2026-04-06 13:33:56 +0300

    add automate for test scrapers

---
+++ apps/frontend/src/app/admin/scraper-test/test-urls.ts
@@ -0,0 +1,34 @@
+/**
+ * Список URL-ів для тестування скрапера.
+ * Додавай нові посилання сюди — вони з'являться на адмін-сторінці.
+ */
+export const TEST_URLS: string[] = [
+  // Rozetka
+  "https://rozetka.com.ua/ua/apple-iphone-15-128gb-black/p/380338427/",
+  "https://rozetka.com.ua/ua/samsung-galaxy-s24-ultra-256gb-titanium-gray/p/396425287/",
+
+  // Epicentr
+  "https://epicentrk.ua/ua/shop/kran-kulkoviy-bugatti-1-2-vn-vn-zhb-art-300.html",
+
+  // Foxtrot
+  "https://www.foxtrot.com.ua/uk/shop/smartfony-apple-iphone-15-128gb-black.html",
+
+  // Prom.ua
+  "https://prom.ua/ua/p2098762853-nabor-posudy-tefal.html",
+
+  // eBay
+  "https://www.ebay.com/itm/125000000001",
+
+  // OLX
+  "https://www.olx.ua/d/uk/obyavlenie/iphone-15-pro-max-256gb-IDxxxx.html",
+
+  // Amazon
+  "https://www.amazon.com/dp/B0CHX3QBCH",
+
+  // AliExpress
+  "https://www.aliexpress.com/item/1005006123456789.html",
+
+  // Generic sites (JSON-LD / meta fallback)
+  "https://www.nike.com/t/air-max-90-mens-shoes-6n3vKB",
+  "https://www.apple.com/shop/buy-iphone",
+];
